010ce9b3866d448b3111c2c25a22a175224b4256
VivekShahare04 <mastervivek23@gmail.com>
2025-03-30 13:04:28 +0530

    updated

--- frontend/src/components/LocationSearchPanel.jsx
+++ frontend/src/components/LocationSearchPanel.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const LocationSearchPanel = (props) => {
-    console.log(props);
+    //console.log(props);
     const Locations = [
         'vivek coding school',
         'pushkar coding school',
